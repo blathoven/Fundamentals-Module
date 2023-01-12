ad2af99a7ce9b123082c98d92a1755dc1c8e0ddc
Blagovest Mitsev <blagovest_micev@abv.bg>
2023-01-12 11:31:16 +0200

    Add files via upload

---
+++ RegularExpressions/MatchFullName.java
@@ -0,0 +1,21 @@
+package RegularExpressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Pattern pattern = Pattern.compile("\\b[A-Z][a-z]+[ ][A-Z][a-z]+\\b");
+
+        String inputLine = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(inputLine);
+
+        while (matcher.find()) {
+            System.out.print(matcher.group() + " ");
+        }
+    }
+}
